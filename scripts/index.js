ec0901c4eef1da1355c943d5a4421c09f85d1b16
Biokarl <Justarch1@yandex.ru>
2024-01-24 11:43:19 +0300

    К ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,23 @@
 // @todo: Темплейт карточки
-
 const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 const cardsContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
-function addCard(cardText, cardImg, index) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+function addCard(cardText, cardImg) {
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
   cardElement.querySelector(".card__title").textContent = cardText;
   cardElement.querySelector(".card__image").src = cardImg;
 
-  cardElement.querySelector(".card__delete-button").addEventListener("click", function (evt) {
-    const cards = document.querySelectorAll(".card");
-    cards[index].remove();
-    console.log(cards);
-  });
-
+  cardElement.querySelector(".card__delete-button").addEventListener("click", deleteCard);
   cardsContainer.append(cardElement);
 }
-
 // @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-function renderCards() {
-  initialCards.forEach(function (el, index) {
-    addCard(el.name, el.link, index);
-  });
+function deleteCard() {
+  this.parentElement.remove();
 }
-renderCards();
+// @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (item) {
+  addCard(item.name, item.link);
+});
