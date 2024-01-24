23ba4ce6ce788b722422e99d0c4e030b60f29fcc
Biokarl <Justarch1@yandex.ru>
2024-01-24 11:52:45 +0300

    Рефакторинг кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,29 @@
 // @todo: Темплейт карточки
+
 const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+
 const cardsContainer = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
+
 function addCard(cardText, cardImg) {
   const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+
   cardElement.querySelector(".card__title").textContent = cardText;
   cardElement.querySelector(".card__image").src = cardImg;
-
   cardElement.querySelector(".card__delete-button").addEventListener("click", deleteCard);
+
   cardsContainer.append(cardElement);
 }
+
 // @todo: Функция удаления карточки
+
 function deleteCard() {
   this.parentElement.remove();
 }
+
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (item) {
